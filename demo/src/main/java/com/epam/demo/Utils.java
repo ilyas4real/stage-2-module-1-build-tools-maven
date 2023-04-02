54db80780540fdc2062450172c95fa2e0f3b0ed4
ilyas4real <iliyas.ysmaiyl@gmail.com>
2023-04-02 11:40:48 +0200

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,16 @@
 package com.epam.demo;
 
+import com.epam.utils.StringUtils;
+
 import java.util.List;
+import java.util.Optional;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        //magic happens here
+        Optional<String> firstNegative = args.stream()
+                .filter(num -> !StringUtils.isPositiveNumber(num))
+                .findFirst();
+        return firstNegative.isEmpty();
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,5 +3,11 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         //code!
+        try {
+            int num = Integer.parseInt(str);
+            return num > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
